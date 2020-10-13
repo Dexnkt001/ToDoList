5b133a58b502bb2080018fa89980c83b2a0bb778
Dexnkt001 <48957792+Dexnkt001@users.noreply.github.com>
2020-10-13 14:50:37 +0300

    start: start work

---
+++ todolist/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="todoContainer">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
